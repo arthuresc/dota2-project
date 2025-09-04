8acf7ff34238a914dba5fef06a307516a6c1f18f
Arthur Escalera <arthurescalera@gmail.com>
2025-09-04 16:21:37 -0300

    feat(rotas): Aplicado o modelo mais basico de rotas para entendimento do seu funcionamento

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './style/index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './style/index.css';
+import App from './App.tsx';
+let router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+  },
+]);
 
 // ! é o not null deles, é um valor que não pode ser nulo, por isso se tirar ele continua a funcionar mas pode dar problema no futuro
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* // <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
